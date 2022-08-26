6ccb49820027e85407cd799e6c5ac7e1553612b1
hugengsetopranowo <hugengseto@gmail.com>
2022-08-26 14:55:28 +0700

    dialog jquery

---
+++ docs/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+  //printah detail
+  $("#detail").click(function () {
+    $("#biografi").slideDown("normal");
+    $("#bg").fadeIn(1000);
+    $(".kartu").css("position", "absolute");
+    $(".kartu").css("display", "none");
+  });
+  $("#detail1").click(function () {
+    $("#biografi1").slideDown("normal");
+    $("#bg").fadeIn(1000);
+    $(".kartu").css("position", "absolute");
+    $(".kartu").css("display", "none");
+  });
+  $("#detail2").click(function () {
+    $("#biografi2").slideDown("normal");
+    $("#bg").fadeIn(1000);
+    $(".kartu").css("position", "absolute");
+    $(".kartu").css("display", "none");
+  });
+  $("#detail3").click(function () {
+    $("#biografi3").slideDown("normal");
+    $("#bg").fadeIn(1000);
+    $(".kartu").css("position", "absolute");
+    $(".kartu").css("display", "none");
+  });
+  $("#detail4").click(function () {
+    $("#biografi4").slideDown("normal");
+    $("#bg").fadeIn(1000);
+    $(".kartu").css("position", "absolute");
+    $(".kartu").css("display", "none");
+  });
+  $("#detail5").click(function () {
+    $("#biografi5").slideDown("normal");
+    $("#bg").fadeIn(1000);
+    $(".kartu").css("position", "absolute");
+    $(".kartu").css("display", "none");
+  });
+  $("#detail6").click(function () {
+    $("#biografi6").slideDown("normal");
+    $("#bg").fadeIn(1000);
+    $(".kartu").css("position", "absolute");
+    $(".kartu").css("display", "none");
+  });
+
+  //printah close
+  $("#close-button").click(function () {
+    $("#biografi").slideUp("fast");
+    $("#bg").fadeOut(1000);
+    $(".kartu").css("position", "static");
+    $(".kartu").css("display", "block");
+  });
+  $("#close-button1").click(function () {
+    $("#biografi1").slideUp("fast");
+    $("#bg").fadeOut(1000);
+    $(".kartu").css("position", "static");
+    $(".kartu").css("display", "block");
+  });
+  $("#close-button2").click(function () {
+    $("#biografi2").slideUp("fast");
+    $("#bg").fadeOut(1000);
+    $(".kartu").css("position", "static");
+    $(".kartu").css("display", "block");
+  });
+  $("#close-button3").click(function () {
+    $("#biografi3").slideUp("fast");
+    $("#bg").fadeOut(1000);
+    $(".kartu").css("position", "static");
+    $(".kartu").css("display", "block");
+  });
+  $("#close-button4").click(function () {
+    $("#biografi4").slideUp("fast");
+    $("#bg").fadeOut(1000);
+    $(".kartu").css("position", "static");
+    $(".kartu").css("display", "block");
+  });
+  $("#close-button5").click(function () {
+    $("#biografi5").slideUp("fast");
+    $("#bg").fadeOut(1000);
+    $(".kartu").css("position", "static");
+    $(".kartu").css("display", "block");
+  });
+  $("#close-button6").click(function () {
+    $("#biografi6").slideUp("fast");
+    $("#bg").fadeOut(1000);
+    $(".kartu").css("position", "static");
+    $(".kartu").css("display", "block");
+  });
+});
